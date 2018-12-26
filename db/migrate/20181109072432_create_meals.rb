772081bbbc6384965a75efdfa616e163477f94ef
nahidebnahasan <nahidebnahasan@gmail.com>
2018-12-27 01:59:09 +0600

    show meal details data on the home page

--- db/migrate/20181109072432_create_meals.rb
+++ db/migrate/20181109072432_create_meals.rb
@@ -3,7 +3,7 @@ class CreateMeals < ActiveRecord::Migration[5.2]
     create_table :meals do |t|
       t.integer :user_id
       t.integer :group_id
-      t.date    :date
+      t.date :date
       t.boolean :breakfast_done, default: false
       t.boolean :lunch_done, default: false
       t.boolean :dinner_done, default: false
@@ -12,7 +12,7 @@ class CreateMeals < ActiveRecord::Migration[5.2]
       t.column :num_dinner_taken, :decimal, precision: 10, scale: 2
       t.timestamps
     end
-    add_index :meals, :user_id,  unique: true
+    add_index :meals, :user_id, unique: true
     add_index :meals, :group_id, unique: true
   end
 end
