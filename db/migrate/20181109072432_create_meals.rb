c732d312b6b117a22a58b11d0f2338cb0c03b529
nahidebnahasan <nahidebnahasan@gmail.com>
2019-04-03 13:42:34 +0600

    modified migration file for meals

--- db/migrate/20181109072432_create_meals.rb
+++ db/migrate/20181109072432_create_meals.rb
@@ -7,9 +7,9 @@ class CreateMeals < ActiveRecord::Migration[5.2]
       t.boolean :breakfast_done, default: false
       t.boolean :lunch_done, default: false
       t.boolean :dinner_done, default: false
-      t.column :num_breakfast_taken, :decimal, precision: 10, scale: 2
-      t.column :num_lunch_taken, :decimal, precision: 10, scale: 2
-      t.column :num_dinner_taken, :decimal, precision: 10, scale: 2
+      t.column :breakfast_count, :decimal, precision: 10, scale: 2
+      t.column :lunch_count, :decimal, precision: 10, scale: 2
+      t.column :dinner_count, :decimal, precision: 10, scale: 2
       t.timestamps
     end
     add_index :meals, :user_id, unique: true
